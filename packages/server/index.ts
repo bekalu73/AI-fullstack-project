d81fb1ff24bb520be9803ad7f6de1e9a74cd9e6e
bekalu73 <bekalusisay2010@gmail.com>
2025-09-09 15:56:36 +0300

    feat:manage openai api key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port http://localhost:${PORT}`);
+});
