23f6fc8571147dbcd5f04b634e9e9e1f7c1125fa
bekalu73 <bekalusisay2010@gmail.com>
2025-09-19 10:38:16 +0300

    setup husky

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,13 @@
-import express, { type Request, type Response } from "express";
+import express, { type Request, type Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // routes
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json("Hello World");
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json('Hello World');
 });
 
 app.listen(PORT, () => {
-  console.log(`server is running on port http://localhost:${PORT}`);
+   console.log(`server is running on port http://localhost:${PORT}`);
 });
