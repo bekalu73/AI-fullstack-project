ae1b5085d4870a9878ed440d02467411f407fb2e
bekalu73 <bekalusisay2010@gmail.com>
2025-09-19 09:47:50 +0300

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,8 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // routes
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json("Hello World");
 });
 
 app.listen(PORT, () => {
